959833d7a587d9bd2d7cdd7b8491d7672151f69b
TraffordFewster <82023031+TraffordFewster@users.noreply.github.com>
2021-11-05 12:35:19 +0000

    PSR-12ify Age Rating Multiplier

--- src/Service/Models/AgeRatingMultiplier.php
+++ src/Service/Models/AgeRatingMultiplier.php
@@ -1,22 +1,63 @@
 <?php
+/**
+ * Age Rating Multiplier
+ * The Age Rating Multiplier class file.
+ * PHP Version 8.0.2
+ * 
+ * @category Multipliers
+ * @package  QuoteEngine
+ * @author   Trafford Fewster <contact@trafford.dev>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/TraffordFewster/PriceCalculator
+ */
 namespace App\Service\Models;
 
 use App\Service\Storage\Database;
 
+/**
+ * AgeRatingMultiplier class
+ * A simple class using the MultiplierInterface to provide the multiplier for Age
+ * 
+ * @category Multipliers
+ * @package  QuoteEngine
+ * @author   Trafford Fewster <contact@trafford.dev>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/TraffordFewster/PriceCalculator
+ */
 class AgeRatingMultiplier implements MultiplierInterface
 {
-    private $age = "";
+    private $_age = "";
 
+    /**
+     * Constructor
+     * The constructor to setup the class.
+     * 
+     * @param string $age The age to check.
+     */
     public function __construct(string $age)
     {
         $this->setValue($age);
     }
 
+    /**
+     * Set Value
+     * Sets the value of the age.
+     *
+     * @param string $value the value to set the registration too
+     * 
+     * @return void
+     */
     public function setValue(string $value)
     {
-        $this->age = $value;
+        $this->_age = $value;
     }
 
+    /**
+     * Get Multiplier
+     * Gets the multiplier for the age provided.
+     *
+     * @return float
+     */
     public function getMultiplier()
     {
         $sql = 'SELECT rating_factor
@@ -24,7 +65,7 @@ class AgeRatingMultiplier implements MultiplierInterface
                 WHERE age = :age';
         $db = new Database();
         $query = $db->prepare($sql);
-        $query->execute([":age" => $this->age]);
+        $query->execute([":age" => $this->_age]);
         $result = $query->fetchColumn();
         return $result;
     }
