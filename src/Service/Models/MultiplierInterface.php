d5aabe620e1bda98c3d8d20360f816db53d923eb
TraffordFewster <82023031+TraffordFewster@users.noreply.github.com>
2021-11-05 10:57:39 +0000

    DocBlock Multiplier Interface

--- src/Service/Models/MultiplierInterface.php
+++ src/Service/Models/MultiplierInterface.php
@@ -1,8 +1,44 @@
 <?php
+/**
+ * MultiplierInterface
+ * The File containing the Interface for all multipliers
+ * PHP Version 8.0.2
+ * 
+ * @category Multipliers
+ * @package  QuoteEngine
+ * @author   Trafford Fewster <contact@trafford.dev>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/TraffordFewster/PriceCalculator
+ */
 namespace App\Service\Models;
 
+/**
+ * MultiplierInterface
+ * The Interface for all multipliers
+ * 
+ * @category Multipliers
+ * @package  QuoteEngine
+ * @author   Trafford Fewster <contact@trafford.dev>
+ * @license  MIT https://opensource.org/licenses/MIT
+ * @link     https://github.com/TraffordFewster/PriceCalculator
+ */
 interface MultiplierInterface
 {
+    /**
+     * Set Value
+     * Used to set the value the multiplier requires
+     * 
+     * @param string $value the used when getting the multiplier.
+     * 
+     * @return void
+     */
     public function setValue(string $value);
+
+    /**
+     * Get Multiplier
+     * Used to get the multiplier after the value is set.
+     * 
+     * @return float the multiplier.
+     */
     public function getMultiplier();
 }
